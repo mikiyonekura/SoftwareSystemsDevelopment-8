70c6086c548028bf683a4fa4140dd40cb3274611
yonekuramiki <mikiyonekura4040@gmail.com>
2024-01-12 19:35:09 +0900

    Add imu2

--- src/persistence/sqlite.js
+++ src/persistence/sqlite.js
@@ -44,7 +44,7 @@ async function getItems() {
             acc(
                 rows.map(item =>
                     Object.assign({}, item, {
-                        // completed: item.completed === 1,
+                        comppleted: item.completed === 1,
                     }),
                 ),
             );
