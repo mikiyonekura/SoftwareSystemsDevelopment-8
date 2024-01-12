2db1318b0d8bf1ea749fddf718ba7c2c333d33db
yonekuramiki <mikiyonekura4040@gmail.com>
2024-01-12 19:46:48 +0900

    reset

--- src/persistence/sqlite.js
+++ src/persistence/sqlite.js
@@ -44,7 +44,7 @@ async function getItems() {
             acc(
                 rows.map(item =>
                     Object.assign({}, item, {
-                        comppleted: item.completed === 1,
+                        completed: item.completed === 1,
                     }),
                 ),
             );
